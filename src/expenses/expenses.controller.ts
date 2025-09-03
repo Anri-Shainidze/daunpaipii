1a7b11a2810ad1e8814fef8939531a6c19915a87
Anri-Shainidze <anrianu2006@gmail.com>
2025-09-04 01:37:57 +0400

    dsad

---
+++ src/expenses/expenses.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { ExpensesService } from './expenses.service';
+import { ProductDto } from './ssss/create.expense.dto';
+import { ValidatePricePipe } from './pipes/price-validate.pipe';
+
+@Controller('products')
+export class ExpensesController {
+  constructor(private readonly productsService: ExpensesService) {}
+
+  @Get()
+  getAllProducts(
+    @Query('price', ValidatePricePipe) price?: number,
+    @Query('category') category?: string,
+  ) {
+    return this.productsService.getAllProducts({ price, category });
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') id: string) {
+    return this.productsService.getProductById(id);
+  }
+
+  @Post()
+  createProduct(@Body() body: Omit<ProductDto, 'id' | 'createdAt'>) {
+    return this.productsService.createProduct(body);
+  }
+
+  @Put(':id')
+  updateProduct(@Param('id') id: string, @Body() body: Partial<ProductDto>) {
+    return this.productsService.updateProduct(id, body);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productsService.deleteProduct(id);
+  }
+}
---
+++ src/expenses/expenses.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { ProductDto } from './ssss/create.expense.dto';
+
+@Injectable()
+export class ExpensesService {
+  private products: ProductDto[] = [];
+
+  getAllProducts(filters?: { price?: number; category?: string }) {
+    let result = this.products;
+
+    if (typeof filters?.price === 'number') {
+      result = result.filter((p) => p.price > (filters.price as number));
+    }
+
+    if (filters?.category) {
+      result = result.filter(
+        (p) => p.category.toLowerCase() === filters.category!.toLowerCase(),
+      );
+    }
+
+    return result;
+  }
+
+  getProductById(id: string) {
+    return this.products.find((p) => p.id === id);
+  }
+
+  createProduct(data: Omit<ProductDto, 'id' | 'createdAt'>) {
+    const newProduct: ProductDto = {
+      id: Math.random().toString(),
+      createdAt: new Date(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  updateProduct(id: string, updateData: Partial<ProductDto>) {
+    const productIndex = this.products.findIndex((p) => p.id === id);
+    if (productIndex === -1) return null;
+
+    this.products[productIndex] = {
+      ...this.products[productIndex],
+      ...updateData,
+    };
+
+    return this.products[productIndex];
+  }
+
+  deleteProduct(id: string) {
+    this.products = this.products.filter((p) => p.id !== id);
+    return 'Product deleted';
+  }
+}
